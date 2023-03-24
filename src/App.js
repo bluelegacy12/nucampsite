79297d603dc1bc4e50c8df30994f140bcdab59be
Elza <dylan.elza@gmail.com>
2023-03-24 17:38:36 -0400

    installation files and conversion to single page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CampsitesList from './features/campsites/CampsitesList.js';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
